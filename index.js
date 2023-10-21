2ccb69f4eb903cd963221bec52d356bd6a5a6f8f
deepak013 <deepak89122@gmail.com>
2023-10-21 10:19:18 -0700

    comment

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
-console.log("hello worlsiiufiushdisugfisifugfuhid")
-console.log("sfkasgdiflgaiysgdf")
-console.log("adfjgaljgdfkjagdkjfshj")
+// all object methods
+// obbject.keys
+
+// const obj = {
+//     namr:"sdfkjhkalsdhf",
+//     val:"sadfkashdf"
+// }
+// const newj = Object.keys(obj).map((item)=>{
+//     return item+"z"
+// })
+// const newb = Object.values(obj).map((item)=>{
+//     return item
+// })
+// console.log(newb)
+
+// object.entries return key and value pair return 
+
+
+// const Entry = Object.entries(obj)
+// console.log(Entry)
+
+
+// shallow copy concept
+
+// const target = {
+//     a:1,
+//     b:4,
+//     c:7,
+//     d:9
+// }
+// const source = {
+//     b:6,
+//     c:7
+// }
+// const source2 = {
+//     c:9,
+//     d:6
+// }
+// console.log(Object.assign(target,source,source2))
+
+// shallow copy --- original object copy new object doesnot affect  [affect]
+
+// Object.create  method 
+
+// const oldObj = {
+//     func(){
+//         console.log("asdfmlkhasgdflgalskhdfglhagsdf")
+//     },
+//     kk:{
+//         yoo:"askjdhkashd"
+//     }
+// }
+
+// const newObj = Object.create(oldObj,{
+//     name:{
+//         yoo:"asdjaksd",
+//         jk:"asdjhkajsd"
+//     },
+//     okk:{
+//         name:"sdfkahsdkf",
+//         sdfsd:"sadfjkja"
+//     }
+// })
+// console.log(newObj.kk.yoo)
